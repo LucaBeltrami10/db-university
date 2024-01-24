210f388c362d0885d5dfbbf91c5094d803928563
LucaBeltrami10 <Lucabeltrami10@gmail.com>
2024-01-24 18:41:12 +0100

    prove su input in test

--- test/index.php
+++ test/index.php
@@ -1,4 +1,33 @@
-<?php ?>
+<?php
+
+
+define("DB_SERVER", "localhost");
+define("DB_USERNAME", "root");
+define("DB_PASSWORD", "root");
+define("DB_NAME", "negozio_libri");
+
+$connection = new mysqli(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
+
+if ($connection && $connection->connect_error) {
+    echo "errore di connessione:" . $connection->connect_error;
+}
+
+$sql = "SELECT * FROM `libro`";
+$result = $connection->query($sql);
+
+if ($result && $result->num_rows > 0) {
+    while ($row = $result->fetch_assoc()) {
+        echo $row['titolo'] . "</br>";
+    }
+} elseif ($result) {
+    echo "0 results";
+} else {
+    echo "query error";
+}
+
+$connection->close();
+
+?>
 
 
 <!DOCTYPE html>
@@ -14,18 +43,18 @@
 
 <body>
 
-    <form>
+    <!-- <form class="w-25">
         <div class="mb-3">
-            <label for="exampleInputEmail1" class="form-label">Email address</label>
-            <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
-            <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+            <label for="password" class="form-label">Email address</label>
+            <input type="email" class="form-control" id="email" name="email">
         </div>
         <div class="mb-3">
-            <label for="exampleInputPassword1" class="form-label">Password</label>
-            <input type="password" class="form-control" id="exampleInputPassword1">
+            <label for="password" class="form-label">Password</label>
+            <input type="password" class="form-control" id="password" name="password">
         </div>
         <button type="submit" class="btn btn-primary">Submit</button>
-    </form>
+    </form> -->
+
 
 </body>
 
